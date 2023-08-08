587e9b414248fa7e7e37e60b199efdd7ff399268
Lautaro Tomás Israel Petelin <lautisrra@gmail.com>
2023-08-08 20:17:32 -0300

    Saludo a Pablo
    sout

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -5,6 +5,7 @@ public class PruebaGit {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo PABLO");
+        System.out.println("Hola mundo LAUTY");
     }
     
 }
