870c1b6fdca1f9f5f3f177fec0a38dea88fd385f
Lautaro Tomás Israel Petelin <lautisrra@gmail.com>
2023-08-08 20:47:43 -0300

    Borre espacios innecesarios

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -6,9 +6,6 @@ public class PruebaGit {
     public static void main(String[] args) {
         System.out.println("Hola mundo PABLO");
         System.out.println("Hola mundo LAUTY");
-        System.out.println("Eze tiene calor");
-        
-        
+        System.out.println("Eze tiene calor");   
     }
-    
 }
